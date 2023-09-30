457216d36f543cb3e1010f03c0cdec7318f4a68c
Jake Willman <jakenwillman@gmail.com>
2023-09-29 19:54:17 -0500

    completed Task 6

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,54 +2,56 @@ package org.launchcode.techjobs.oo;
 
 import java.util.Objects;
 
-public class Employer {
+// Modify to extend the JobField class
+public class Employer extends JobField {
 
-    private int id;
-    private static int nextId = 1;
-    private String value;
+//    private int id;
+//    private static int nextId = 1;
+//    private String value;
+//
+//    public Employer() {
+//        id = nextId;
+//        nextId++;
+//    }
 
-    public Employer() {
-        id = nextId;
-        nextId++;
-    }
-
+    // Replace the original shared constructors using super and passing in the (value)
     public Employer(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
+}
 
     // Custom toString, equals, and hashCode methods:
 
-    @Override
-    public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-        if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o;
-        return getId() == employer.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-}
+//    @Override
+//    public String toString() {
+//        return value;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+//        if (this == o) return true;
+//        if (!(o instanceof Employer)) return false;
+//        Employer employer = (Employer) o;
+//        return getId() == employer.getId();
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(getId());
+//    }
+//
+//    // Getters and Setters:
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(String value) {
+//        this.value = value;
+//    }
+//
+//}
---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,58 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+// Declare each of the common fields shared between Employer, Location, Core Competency, and Position Type classes
+// Make the JobField class an abstract class to prevent the creation of a JobField object
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    // Code the common repeated constructors
+    // 1st constructor
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+    // 2nd constructor
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    // Use generate to create all the appropriate getters and setters that are reused between the other classes
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    // Generate the reused equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JobField jobField = (JobField) o;
+        return id == jobField.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Create the commonly reused toString method
+    @Override
+    public String toString() {
+        return value;
+    }
+}
+
+
